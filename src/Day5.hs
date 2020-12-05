861e220fc025c919ba5b181db3ce265ddb5263df
Joni Nevalainen <joninevalainen@gmail.com>
2020-12-05 11:15:11 +0200

    Day 5 b

--- src/Day5.hs
+++ src/Day5.hs
@@ -1,9 +1,8 @@
 module Day5 where
 
 import Data.Foldable (maximumBy)
+import Data.List (sort)
 import Data.Ord (comparing)
-
-data Direction = UP | DOWN
 
 parseRow :: String -> [Int]
 parseRow s = foldr f [0 .. 127] (reverse s)
@@ -36,8 +35,13 @@ parseColumnNumber s = case parseColumn s of
 getBoardingPassId :: String -> Int
 getBoardingPassId s = (\(r, c) -> parseRowNumber r * 8 + parseColumnNumber c) $ splitAt 7 s
 
+findMySeatId :: [Int] -> Int
+findMySeatId ids = (+ 1) $ fst $ head $ filter (\(a, b) -> b - a /= 1) $ zip sortedIds (tail sortedIds)
+  where
+    sortedIds = sort ids
+
 solveA :: String -> String
 solveA s = show $ snd $ maximumBy (comparing snd) $ map (\v -> (v, getBoardingPassId v)) $ lines s
 
 solveB :: String -> String
-solveB s = s
+solveB s = show $ findMySeatId $ map getBoardingPassId $ lines s
