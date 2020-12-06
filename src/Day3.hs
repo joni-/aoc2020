a017652690e84bfdabbdba8187baf3bc0a5b3476
Joni Nevalainen <joninevalainen@gmail.com>
2020-12-06 20:39:41 +0200

    Slight cleaning for day 3

--- src/Day3.hs
+++ src/Day3.hs
@@ -10,14 +10,14 @@ getPositions (deltaX, deltaY) grid = positions
     positions = [(i * deltaX, i * deltaY) | i <- [1 .. (length grid)]]
 
 hasTree :: Grid -> Position -> Bool
-hasTree grid (x, y) = if y >= length grid then False else hasTree
+hasTree grid (x, y) = y < length grid && hasTree'
   where
     row = grid !! y
     x' = x `mod` length row
-    hasTree = row !! x' == '#'
+    hasTree' = row !! x' == '#'
 
 countTrees :: Position -> Grid -> Int
-countTrees slope grid = length $ filter (== True) $ map (\p -> hasTree grid p) $ positions
+countTrees slope grid = length $ filter (== True) $ map (hasTree grid) positions
   where
     positions = getPositions slope grid
 
@@ -25,7 +25,7 @@ solveA :: String -> String
 solveA s = show $ countTrees (3, 1) $ lines s
 
 solveB :: String -> String
-solveB s = show $ product $ fmap (\slope -> countTrees slope grid) slopes
+solveB s = show $ product $ fmap (`countTrees` grid) slopes
   where
     slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
     grid = lines s
