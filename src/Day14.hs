09f3aee382767f52396bb733cef40eb825c93fa7
Joni Nevalainen <joninevalainen@gmail.com>
2020-12-15 20:38:35 +0200

    Day 14 b

--- src/Day14.hs
+++ src/Day14.hs
@@ -1,17 +1,16 @@
 module Day14 where
 
+import Control.Monad (replicateM)
 import Data.Char (digitToInt, intToDigit)
+import Data.Foldable (toList)
+import Data.List (findIndices)
 import qualified Data.Map as M
 import Data.Maybe (listToMaybe, mapMaybe)
+import qualified Data.Sequence as Seq
 import Numeric (readInt, showIntAtBase)
 import Parser (between, parseInt)
 
-input' =
-  "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X\n\
-  \mem[8] = 11\n\
-  \mem[7] = 101\n\
-  \mem[8] = 0\n"
-
+-- stolen from stack overflow
 readBin :: Integral a => String -> Maybe a
 readBin = fmap fst . listToMaybe . readInt 2 (`elem` "01") digitToInt
 
@@ -27,11 +26,32 @@ type State = (Maybe Mask, Memory)
 
 data Command = SetMask Mask | UpdateMemory Integer Integer deriving (Show)
 
-applyMask :: Mask -> Integer -> Maybe Integer
-applyMask mask value = fmap toInteger $ readBin $ zipWith (curry f) mask valueS
+applyMaskV1 :: Mask -> Integer -> Maybe Integer
+applyMaskV1 mask value = fmap toInteger $ readBin $ zipWith (curry f) mask valueS
   where
     valueS = lpad 36 '0' $ showIntAtBase 2 intToDigit value ""
     f (m, c) = if m == 'X' then c else m
+
+replaceXs :: String -> [String]
+replaceXs input = map applyReplacements allReplacements
+  where
+    -- find indices of Xs to replace in the input
+    indices = findIndices (`elem` "X") input
+    count = length indices
+    -- all permutations of length "count" of 0s and 1s
+    permutations = replicateM count "01"
+    -- build a list of list of replacements where the inner list tells the
+    -- replacement pattern for one permutation
+    allReplacements = map (zip indices) permutations
+    applyReplacements replacements = toList $ foldl (\acc (i, c) -> Seq.update i c acc) (Seq.fromList input) replacements
+
+applyMaskV2 :: Mask -> Integer -> [Integer]
+applyMaskV2 mask value = mapMaybe readBin (replaceXs $ zipWith (curry f) mask valueS)
+  where
+    valueS = lpad 36 '0' $ showIntAtBase 2 intToDigit value ""
+    f ('0', c) = c
+    f ('1', _) = '1'
+    f (m, _) = m
 
 parseCommand :: String -> Maybe Command
 parseCommand command = case command of
@@ -42,20 +62,33 @@ parseCommand command = case command of
       Nothing -> Nothing
     Nothing -> Nothing
 
-updateState :: State -> Command -> State
-updateState (mask, memory) command = case command of
-  SetMask mask' -> (Just mask', memory)
-  UpdateMemory address value -> case mask of
-    Just mask' -> case applyMask mask' value of
-      Just value' -> (mask, M.insert address value' memory)
-      Nothing -> (mask, M.insert address value memory)
+updateStateV1 :: State -> Integer -> Integer -> State
+updateStateV1 (mask, memory) address value = case mask of
+  Just mask' -> case applyMaskV1 mask' value of
+    Just value' -> (mask, M.insert address value' memory)
     Nothing -> (mask, M.insert address value memory)
+  Nothing -> (mask, memory)
 
-solveA :: String -> String
-solveA s = show $ sum $ map snd $ M.toList memory
+updateStateV2 :: State -> Integer -> Integer -> State
+updateStateV2 (mask, memory) address value = case mask of
+  Just mask' -> (mask, foldl (\mem addr -> M.insert addr value mem) memory addresses)
+    where
+      addresses = applyMaskV2 mask' address
+  Nothing -> (mask, memory)
+
+runCommand :: (State -> Integer -> Integer -> State) -> State -> Command -> State
+runCommand updateFn (mask, memory) command = case command of
+  SetMask mask' -> (Just mask', memory)
+  UpdateMemory address value -> updateFn (mask, memory) address value
+
+solve' :: (State -> Command -> State) -> String -> String
+solve' updateFn s = show $ sum $ map snd $ M.toList memory
   where
     commands = mapMaybe parseCommand $ lines s
-    (_, memory) = foldl updateState (Nothing, M.empty) commands
+    (_, memory) = foldl updateFn (Nothing, M.empty) commands
+
+solveA :: String -> String
+solveA = solve' (runCommand updateStateV1)
 
 solveB :: String -> String
-solveB s = s
+solveB = solve' (runCommand updateStateV2)
--- test/Day14Test.hs
+++ test/Day14Test.hs
@@ -4,9 +4,21 @@ import Day14
 import Test.Tasty
 import Test.Tasty.HUnit (testCase, (@?=))
 
+input1 =
+  "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X\n\
+  \mem[8] = 11\n\
+  \mem[7] = 101\n\
+  \mem[8] = 0\n"
+
+input2 =
+  "mask = 000000000000000000000000000000X1001X\n\
+  \mem[42] = 100\n\
+  \mask = 00000000000000000000000000000000X0XX\n\
+  \mem[26] = 1"
+
 tests =
   testGroup
     "Day14"
-    [ testCase "solveA" $ solveA "hello" @?= "hello",
-      testCase "solveB" $ solveB "hello" @?= "hello"
+    [ testCase "solveA" $ solveA input1 @?= "165",
+      testCase "solveB" $ solveB input2 @?= "208"
     ]
